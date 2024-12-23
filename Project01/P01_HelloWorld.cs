947204d587201e82019fb178aa28177b619c4804
MohammadReza Golchehre <mohammadrezagol84@gmail.com>
2024-12-23 19:29:33 +0330

    s

---
+++ Project01/P01_HelloWorld.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Loading...");
+
+        //یک ثانیه صبر همینجوری 
+        Thread.Sleep(1000);
+
+        //چاپ پیام Hello, World! در خروجی
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+ 
