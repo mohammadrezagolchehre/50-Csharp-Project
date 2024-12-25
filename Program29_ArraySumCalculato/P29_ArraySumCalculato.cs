eebd0e9027cc7f2bc5363768ba4a18fd7ae7c814
MohammadReza Golchehre <mohammadrezagol84@gmail.com>
2024-12-26 00:04:08 +0330

    C# Programs

---
+++ Program29_ArraySumCalculato/P29_ArraySumCalculato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // آرایه نمونه
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // محاسبه مجموع
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        // نمایش مجموع
+        Console.WriteLine("The sum of the array elements is: " + sum);
+    }
+}
---
+++ Program30_PalindromeChecker/P30_PalindromeChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // دریافت ورودی از کاربر
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        // حذف فضاهای خالی و تبدیل به حروف کوچک
+        string cleanedInput = input.Replace(" ", "").ToLower();
+
+        // بررسی پالیندروم بودن
+        bool isPalindrome = true;
+        int length = cleanedInput.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (cleanedInput[i] != cleanedInput[length - i - 1])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // نمایش نتیجه
+        if (isPalindrome)
+            Console.WriteLine("The string is a palindrome.");
+        else
+            Console.WriteLine("The string is not a palindrome.");
+    }
+}
